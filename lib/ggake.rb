b8049b6196ea0987ef683f0077ea98594a4d1a14
Giles Alexander <giles.alexander@gmail.com>
2013-08-25 15:41:58 +0100

    Adding a CoffeeScript compilation task.

--- lib/ggake.rb
+++ lib/ggake.rb
@@ -1,5 +1,6 @@
 require "ggake/version"
 
 require "ggake/haml"
+require "ggake/coffee"
 require "ggake/transient"
 require "ggake/cloud_cp"
---
+++ lib/ggake/coffee.rb
@@ -0,0 +1,13 @@
+module Rake
+  module DSL
+
+    def coffee(input, options)
+      (File.join(options[:output], File.basename(input, '.coffee')) + '.js').tap do |js_file|
+        file js_file => input do
+          sh "coffee --compile --output #{options[:output]} #{input}"
+        end
+      end
+    end
+
+  end
+end
