82eccc5e52feaa7948470cdaf3cf2d678a38db0f
Giles Alexander <giles.alexander@gmail.com>
2013-09-03 10:54:06 +0200

    Use that nice little .ext() method from Rake I didn't know about.

--- lib/ggake/haml.rb
+++ lib/ggake/haml.rb
@@ -2,7 +2,7 @@ module Rake
   module DSL
 
     def haml_template(tmpl, options)
-      (File.join(options[:output], File.basename(tmpl, '.haml')) + '.html').tap do |html_file|
+      File.join(options[:output], tmpl).ext('html').tap do |html_file|
         file html_file => tmpl do
           sh "haml #{tmpl} #{html_file}"
         end
