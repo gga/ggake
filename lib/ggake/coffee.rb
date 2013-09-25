664d21853402b63feafbd246054faa6e115b1ef6
Giles Alexander <giles.alexander@gmail.com>
2013-09-25 08:04:53 +0100

    Construct the js file output name for a coffee file correctly.

--- lib/ggake/coffee.rb
+++ lib/ggake/coffee.rb
@@ -2,7 +2,7 @@ module Rake
   module DSL
 
     def coffee(input, options)
-      File.join(options[:output], input).ext('js').tap do |js_file|
+      File.join(options[:output], File.basename(input)).ext('js').tap do |js_file|
         file js_file => input do
           sh "coffee --compile --output #{options[:output]} #{input}"
         end
