434b2cacb17c42ab606c26a9cd0f0e24f50f377b
Giles Alexander <giles.alexander@gmail.com>
2014-05-05 14:12:56 +0100

    When compiling Haml it's useful to be able to load some custom Ruby.

--- lib/ggake/haml.rb
+++ lib/ggake/haml.rb
@@ -2,10 +2,17 @@ module Rake
   module DSL
 
     def haml_template(tmpl, options)
+      extras = ''
+      lib_dir = options[:lib]
+      if lib_dir
+        libraries = Dir["#{lib_dir}/*.rb"].collect { |l| File.basename(l, '*.rb') }
+        extras << "-I#{lib_dir} "
+        extras << libraries.collect { |l| "-r#{l}" }.join(' ')
+      end
       File.join(options[:output], File.basename(tmpl)).ext('html').tap do |html_file|
         directory options[:output]
         file html_file => [options[:output], tmpl] do
-          sh "haml #{tmpl} #{html_file}"
+          sh "haml -t ugly #{extras} #{tmpl} #{html_file}"
         end
       end
     end
